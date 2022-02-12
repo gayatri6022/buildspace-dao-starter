221ed69ae9df688e6820669f08dea2f346bb1346
gayatri6022 <79961916+gayatri6022@users.noreply.github.com>
2022-02-12 17:52:58 +0530

    undoing stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { ThirdwebSDK } from "@3rdweb/sdk";
 import { useEffect, useMemo, useState } from "react";
-import { ethers } from "ethers";
 // import thirdweb
 import { useWeb3 } from "@3rdweb/hooks";
 // We instantiate the sdk on Rinkeby.
@@ -17,67 +16,6 @@ const signer = provider ? provider.getSigner() : undefined;
   const [hasClaimedNFT, setHasClaimedNFT] = useState(false);
   // isClaiming lets us easily keep a loading state while the NFT is minting.
   const [isClaiming, setIsClaiming] = useState(false);
-  // Holds the amount of token each member has in state.
-const [memberTokenAmounts, setMemberTokenAmounts] = useState({});
-// The array holding all of our members addresses.
-const [memberAddresses, setMemberAddresses] = useState([]);
-
-// A fancy function to shorten someones wallet address, no need to show the whole thing.
-const shortenAddress = (str) => {
-  return str.substring(0, 6) + "..." + str.substring(str.length - 4);
-};
-
-// This useEffect grabs all the addresses of our members holding our NFT.
-useEffect(() => {
-  if (!hasClaimedNFT) {
-    return;
-  }
-
-  // Just like we did in the 7-airdrop-token.js file! Grab the users who hold our NFT
-  // with tokenId 0.
-  bundleDropModule
-    .getAllClaimerAddresses("0")
-    .then((addresses) => {
-      console.log("🛏 Members addresses", addresses)
-      setMemberAddresses(addresses);
-    })
-    .catch((err) => {
-      console.error("failed to get member list", err);
-    });
-}, [hasClaimedNFT]);
-
-// This useEffect grabs the # of token each member holds.
-useEffect(() => {
-  if (!hasClaimedNFT) {
-    return;
-  }
-
-  // Grab all the balances.
-  tokenModule
-    .getAllHolderBalances()
-    .then((amounts) => {
-      console.log("👜 Amounts", amounts)
-      setMemberTokenAmounts(amounts);
-    })
-    .catch((err) => {
-      console.error("failed to get token amounts", err);
-    });
-}, [hasClaimedNFT]);
-
-// Now, we combine the memberAddresses and memberTokenAmounts into a single array
-const memberList = useMemo(() => {
-  return memberAddresses.map((address) => {
-    return {
-      address,
-      tokenAmount: ethers.utils.formatUnits(
-        // If the address isn't in memberTokenAmounts, it means they don't
-        // hold any of our token.
-        memberTokenAmounts[address] || 0,
-        18,
-      ),
-    };
-  });
-}, [memberAddresses, memberTokenAmounts]);
 
   useEffect(() => {
      // We pass the signer to the sdk, which enables us to interact with
@@ -108,9 +46,6 @@ const memberList = useMemo(() => {
         setHasClaimedNFT(false);
         console.error("failed to nft balance", error);
       });
-      const tokenModule = sdk.getTokenModule(
-  "0xAAABA73294068C7Bd08FcFFB9c781bE65A542eA6"
-);
   }, [address]);
 
   // This is the case where the user hasn't connected their wallet
